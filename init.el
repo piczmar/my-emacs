e232d1745ae8c40d191492e031933556014add83
Marcin Piczkowski <piczmar@wp.pl>
2020-03-02 03:59:07 +0100

    disable themes

--- init.el
+++ init.el
@@ -1,20 +1,5 @@
 
-;; auto generated
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auto-complete counsel swiper ace-window org-bullets try which-key use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 ;; Our stuff
 
@@ -107,3 +92,13 @@
   (progn
     (ac-config-default)
     (global-auto-complete-mode t)))
+
+;; (use-package color-theme-modern
+;;   :ensure t)
+
+;; (use-package zenburn-theme
+;;   :ensure t
+;;   ;;  :config (load-theme 'zenburn t))
+;;   ;;  :config (load-theme 'subtle-hacker t))
+;;   :config (load-theme 'emacs-21 t))
+
