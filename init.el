4e850bd974221105758aed4682fb8f5a4b2b597b
Marcin Piczkowski <piczmar@wp.pl>
2020-03-03 23:41:17 +0100

    update org config

--- init.el
+++ init.el
@@ -5,9 +5,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-confirm-babel-evaluate nil)
+ '(org-default-notes-file (concat org-directory "~/orgfiles/notes.org"))
+ '(org-directory "~/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-src-fontify-natively t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (expand-region beacon undo-tree elpy flycheck zenburn-theme which-key use-package try org-bullets counsel color-theme-modern color-theme auto-complete ace-window))))
+    (htmlize noflet web-mode iedit ideit expand-region beacon undo-tree elpy flycheck zenburn-theme which-key use-package try org-bullets counsel color-theme-modern color-theme auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,7 +25,6 @@
 
 ;; Our stuff
 
-
 (setq inhibit-startup-message t)
 
 (tool-bar-mode -1)
@@ -25,8 +32,8 @@
 
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
 (package-initialize)
 
@@ -45,10 +52,87 @@
   (which-key-mode))
 
 ;; Org mode stuff
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+(use-package org 
+      :ensure t
+      :pin org)
+
+    (setenv "BROWSER" "chrome")
+    (use-package org-bullets
+      :ensure t
+      :config
+      (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+    
+
+    ;; (setq org-file-apps
+          ;; (append '(
+          ;;           ("\\.pdf\\'" . "evince %s")
+          ;;           ("\\.x?html?\\'" . "/usr/bin/firefox %s")
+          ;;           ) org-file-apps ))
+
+    (global-set-key "\C-ca" 'org-agenda)
+    (setq org-agenda-start-on-weekday nil)
+    (setq org-agenda-custom-commands
+          '(("c" "Simple agenda view"
+             ((agenda "")
+              (alltodo "")))))
+
+    (global-set-key (kbd "C-c c") 'org-capture)
+
+    (setq org-agenda-files (list "~/orgfiles/gcal.org"
+                                 "~/orgfiles/soe-cal.org"
+                                 "~/orgfiles/i.org"
+                                 "~/orgfiles/schedule.org"))
+    (setq org-capture-templates
+          '(("a" "Appointment" entry (file  "~/orgfiles/gcal.org" )
+             "* %?\n\n%^T\n\n:PROPERTIES:\n\n:END:\n\n")
+            ("l" "Link" entry (file+headline "~/orgfiles/links.org" "Links")
+             "* %? %^L %^g \n%T" :prepend t)
+            ("b" "Blog idea" entry (file+headline "~/orgfiles/i.org" "Blog Topics:")
+             "* %?\n%T" :prepend t)
+            ("t" "To Do Item" entry (file+headline "~/orgfiles/i.org" "To Do and Notes")
+             "* TODO %?\n%u" :prepend t)
+            ("m" "Mail To Do" entry (file+headline "~/orgfiles/i.org" "To Do and Notes")
+             "* TODO %a\n %?" :prepend t)
+            ("g" "GMail To Do" entry (file+headline "~/orgfiles/i.org" "To Do and Notes")
+             "* TODO %^L\n %?" :prepend t)
+            ("n" "Note" entry (file+headline "~/orgfiles/i.org" "Notes")
+             "* %u %? " :prepend t)
+
+	    ("s" "Screencast" entry (file "~/orgfiles/screencasts.org")
+	     "* %?\n%i\n")))
+
+    (defadvice org-capture-finalize 
+        (after delete-capture-frame activate)  
+      "Advise capture-finalize to close the frame"  
+      (if (equal "capture" (frame-parameter nil 'name))  
+          (delete-frame)))
+
+    (defadvice org-capture-destroy 
+        (after delete-capture-frame activate)  
+      "Advise capture-destroy to close the frame"  
+      (if (equal "capture" (frame-parameter nil 'name))  
+          (delete-frame)))  
+
+    (use-package noflet
+      :ensure t )
+    (defun make-capture-frame ()
+      "Create a new frame and run org-capture."
+      (interactive)
+      (make-frame '((name . "capture")))
+      (select-frame-by-name "capture")
+      (delete-other-windows)
+      (noflet ((switch-to-buffer-other-window (buf) (switch-to-buffer buf)))
+        (org-capture)))
+;; (require 'ox-beamer)
+;; for inserting inactive dates
+    (define-key org-mode-map (kbd "C-c >") (lambda () (interactive (org-time-stamp-inactive))))
+
+    (use-package htmlize :ensure t)
+
+    (setq org-ditaa-jar-path "~/emacs.d/ditaa/ditaa.jar")
+
+
+
 
 (setq indo-enable-flex-matching t)
 (setq ido-everywhere t)
@@ -68,9 +152,10 @@
     ))
 
 (use-package counsel
-  
-  :ensure t
-  )
+  :bind
+  (("M-y" . counsel-yank-pop)
+   :map ivy-minibuffer-map
+   ("M-y" . ivy-next-line)))
 
 (use-package ivy
   :ensure t
@@ -145,6 +230,21 @@
   :init
   (global-undo-tree-mode))
 
+;; Web mode
+(use-package web-mode
+:ensure t
+:config
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-engines-alist
+'(("django"    . "\\.html\\'")))
+(setq web-mode-ac-sources-alist
+'(("css" . (ac-source-css-property))
+("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+
+(setq web-mode-enable-auto-closing t)
+(setq web-mode-enable-auto-quoting t))
+
+
 
 ;; misceleneaous
 (global-hl-line-mode t)
@@ -158,4 +258,53 @@
   :ensure t
   :config
   (global-set-key (kbd "C-=") 'er/expand-region))
+
+(setq save-interprogram-paste-before-kill t)
+
+(global-auto-revert-mode 1)
+(setq auto-revert-verbose nil)
+(global-set-key (kbd "<f5>") 'revert-buffer)
+
+					; mark and edit all copies of the marked region simultaniously. 
+(use-package iedit
+  :ensure t)
+
+					; if you're windened, narrow to the region, if you're narrowed, widen
+					; bound to C-x n
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
+Intelligently means: region, org-src-block, org-subtree, or defun,
+whichever applies first.
+Narrowing to org-src-block actually calls `org-edit-src-code'.
+
+With prefix P, don't widen, just narrow even if buffer is already
+narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+	((region-active-p)
+	 (narrow-to-region (region-beginning) (region-end)))
+	((derived-mode-p 'org-mode)
+	 ;; `org-edit-src-code' is not a real narrowing command.
+	 ;; Remove this first conditional if you don't want it.
+	 (cond ((ignore-errors (org-edit-src-code))
+		(delete-other-windows))
+	       ((org-at-block-p)
+		(org-narrow-to-block))
+	       (t (org-narrow-to-subtree))))
+	(t (narrow-to-defun))))
+
+;; (define-key endless/toggle-map "n" #'narrow-or-widen-dwim)
+;; This line actually replaces Emacs' entire narrowing keymap, that's
+;; how much I like this command. Only copy it if that's what you want.
+(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
+
+
+
+(defun load-if-exists (f)
+  ""
+  (if (file-readable-p f)
+    (load-file f)))
+
+(load-if-exists "nodes.org")
 
